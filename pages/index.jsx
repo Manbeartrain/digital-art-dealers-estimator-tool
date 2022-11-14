9a71110ede98aac2c759d87ca6eb3e0cb28a9326
Manbeartrain <ahigareda15@gmail.com>
2022-11-14 16:47:30 -0700

    Initialized Project

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="h-screen w-screen bg-blue-600 flex justify-center items-center">
+            <p className="text-3xl text-white font-bold">Hello Next.js</p>
+        </div>
+    );
+}
